9977a4cd71d600aa8419fc3ad054c2eb4dfa6242
royalpinto007 <royalpinto007@gmail.com>
2023-12-09 21:01:13 +0530

    feat: setup anon-aadhar in next

---
+++ client-india/src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
+import { AnonAadhaarProvider } from "anon-aadhaar-react";
+
+const app_id = process.env.NEXT_PUBLIC_APP_ID || "";
+
+export default function App({ Component, pageProps }: AppProps) {
+  const [ready, setReady] = useState<boolean>(false);
+
+  useEffect(() => {
+    setReady(true);
+  }, []);
+
+  return (
+    <>
+      {ready ? (
+        <AnonAadhaarProvider _appId={app_id}>
+          <Component {...pageProps} />
+        </AnonAadhaarProvider>
+      ) : null}
+    </>
+  );
+}
---
+++ client-india/src/pages/index.tsx
@@ -0,0 +1,39 @@
+import Head from "next/head";
+import {
+  AnonAadhaarProof,
+  LogInWithAnonAadhaar,
+  useAnonAadhaar,
+} from "anon-aadhaar-react";
+import { useEffect } from "react";
+
+export default function Home() {
+  // Use the Country Identity hook to get the status of the user.
+  const [anonAadhaar] = useAnonAadhaar();
+
+  useEffect(() => {
+    console.log("Anon Aadhaar: ", anonAadhaar.status);
+  }, [anonAadhaar]);
+
+  return (
+    <div className="min-h-screen bg-gray-100 px-4 py-8">
+      <main className="flex flex-col items-center gap-8 bg-white rounded-2xl max-w-screen-sm mx-auto h-[24rem] md:h-[20rem] p-8">
+        <h1 className="font-bold text-2xl">Welcome to Anon Aadhaar Example</h1>
+        <p>Prove your Identity anonymously using your Aadhaar card.</p>
+
+        {/* Import the Connect Button component */}
+        <LogInWithAnonAadhaar />
+      </main>
+      <div className="flex flex-col items-center gap-4 rounded-2xl max-w-screen-sm mx-auto p-8">
+        {/* Render the proof if generated and valid */}
+        {anonAadhaar?.status === "logged-in" && (
+          <>
+            <p>✅ Proof is valid</p>
+            <p>Got your Aadhaar Identity Proof</p>
+            <>Welcome anon!</>
+            <AnonAadhaarProof code={JSON.stringify(anonAadhaar.pcd, null, 2)} />
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
